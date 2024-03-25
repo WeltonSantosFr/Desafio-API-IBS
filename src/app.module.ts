dcc6eb81771aba20d331833bd62bbca474c363d5
Welton <weltonsantosfr@gmail.com>
2024-03-25 17:41:52 -0300

    create resources and entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AddressModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,7 @@
+export class CreateUserDto {
+    id: string
+    name: string
+    gender: string
+    birthDate: Date
+    maritalStatus: string
+}
